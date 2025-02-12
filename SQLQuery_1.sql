2aec9aeeb7e8d2206564b857eebc692dc79ffc54
martinagg7 <martinagg@myuax.com>
2025-02-12 15:07:28 +0100

    + cambios sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,11 +1,11 @@
 SELECT COLUMN_NAME ,DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME='001_sales';
-----CUIDADO CON EL COUNT
+----Agrupar por producto y contar el numero medio de productos y el precio medio
 SELECT [Id_Producto],COUNT([Id_Producto]) AS Numero_Productos,ROUND(AVG([PVP]),2) AS Precio_Medio
 FROM [DATAEX].[001_sales]
 GROUP BY [Id_Producto];
----
+---Agrupar por prodcto contar los distintos y contrar distintivos y quiar los nulos
 SELECT [Id_Producto],COUNT([Id_Producto]) AS Numero_Productos,
                     COUNT(DISTINCT[Id_Producto]) AS Productos_Unicos,
                     ROUND(AVG(CAST([PVP] AS float)),2) AS Precio_Medio
@@ -15,16 +15,9 @@ WHERE [Id_Producto] IS NOT NULL
 GROUP BY [Id_Producto]
 ;
 
----covertir la fecha a formato texto
+--Ccovertir la fecha a formato texto
 SELECT
     [Sales_Date],
     CAST(CONVERT(DATE,Sales_Date,103)AS DATE) AS Fecha_Convertida
     FROM[DATAEX].[001_sales]
 
---ir a la ayuda de bigquery de azure
-SELECT
-    [Id_Producto],
-    SUM([IMPUESTOS])
-FROM[DATAEX].[001_sales]
-GROUP BY [Id_Producto];
----Trabajar
